882bdb50d19077e4558aee24468c56579e80454f
Harish Ojha <harish0916ojhacs@gmail.com>
2024-07-08 13:49:11 +0530

    UPdated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Friends this is BLOOM....
         </p>
         <a
           className="App-link"
